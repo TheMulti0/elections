336fa2888422ea230ae7db11fdeb1e47a56981fe
TheMulti0 <multi@codeprecise.com>
2020-08-10 12:25:02 +0300

    Upgraded to Angular 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/app/app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { MaterialModule } from './app.material.module.exports';
-import { ResultsComponent } from './components/results/results.component';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { FormsModule } from '@angular/forms';
-import { TableComponent } from './components/table/table.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ResultsComponent,
-    TableComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    MaterialModule,
-    NgxChartsModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
